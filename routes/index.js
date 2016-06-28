f31ebdc08f832230e5fd5e89f50fe838e2c8d95e
Herman Ng <heymanhn@gmail.com>
2016-06-28 16:51:46 -0700

    Update default response on root API endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send('This is the Journey app.');
+  res.status(200).json({
+    success: true,
+    message: 'This is the Journey API.'
+  });
 });
 
 module.exports = router;
