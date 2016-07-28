a29971549dd344dde36d9d09c35de63925be6665
Herman Ng <heymanhn@gmail.com>
2016-07-27 23:12:23 -0700

    Implement GET, PUT, and DELETE for /trips/:tripId
    Management of wishlists and itineraries will come at a later commit.
    [Finishes #127139351]

--- routes/v1/trips.js
+++ routes/v1/trips.js
@@ -1,6 +1,7 @@
 /*jslint node: true */
 'use strict';
 
+var _ = require('underscore');
 var express = require('express');
 
 var ensureAuth = require('../../utils/auth').ensureAuth;
@@ -39,5 +40,108 @@ app.post('/', ensureAuth, function(req, res, next) {
     })
     .catch(next);
 });
+
+/*
+ * GET /trips/:tripId
+ *
+ * Gets details about a particular trip created by the currently authenticated
+ * user.
+ *
+ */
+app.get('/:tripId', ensureAuth, function(req, res, next) {
+  findTrip(req.params.tripId, req.user._id)
+    .then(function(trip) {
+      res.json({
+        trip: trip
+      });
+    })
+    .catch(next);
+});
+
+/*
+ * PUT /trips/:tripId
+ *
+ * Updates the trip. Only allowed on trips created by currently authenticated
+ * user.
+ *
+ * COMING SOON: Figure out how to set and update the wishlist and itinerary
+ * properties
+ *
+ */
+app.put('/:tripId', ensureAuth, function(req, res, next) {
+  findTrip(req.params.tripId, req.user._id)
+    .then(updateTrip.bind(null, req.body))
+    .then(saveTrip)
+    .then(function(trip) {
+      res.json({
+        message: 'Trip updated successfully',
+        trip: trip
+      });
+    })
+    .catch(next);
+});
+
+/*
+ * DELETE /trips/:tripId
+ *
+ * Deletes the trip. Only allowed on trips created by currently authenticated
+ * user.
+ *
+ */
+app.delete('/:tripId', ensureAuth, function(req, res, next) {
+  findTrip(req.params.tripId, req.user._id)
+    .then(removeTrip)
+    .then(function() {
+      res.json({
+        message: 'Trip deleted.'
+      });
+    })
+    .catch(next);
+});
+
+function findTrip(tripId, userId) {
+  var params = {
+    _id: tripId,
+    creator: userId
+  };
+
+  return Trip
+    .findOne(params)
+    .exec()
+    .then(function(trip) {
+      if (!trip) {
+        var err = new Error('Trip not found');
+        err.status = 404;
+        return Promise.reject(err);
+      }
+
+      return trip;
+    });
+}
+
+function updateTrip(params, trip) {
+  var newParams = {
+    title: params.title,
+    destinations: params.destinations,
+    visibility: params.visibility
+  };
+
+  // Only keep the params that need to be modified
+  _.each(newParams, function(value, key) {
+    if (value !== undefined && value !== trip[key]) {
+      trip[key] = value;
+    }
+  });
+
+  return trip;
+}
+
+function saveTrip(trip) {
+  return trip.save();
+}
+
+function removeTrip(trip) {
+  return trip.remove();
+}
 
 module.exports = app;
