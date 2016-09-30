27f413ae2a2617f612791e906f64b8adce92ea10
Herman Ng <heymanhn@gmail.com>
2016-09-30 14:58:05 -0700

    Fix test warnings in userModel and update syntax to ES6
    [#131461237]

--- tests/models/userModel.test.js
+++ tests/models/userModel.test.js
@@ -1,64 +1,60 @@
 /*jslint node: true, mocha: true */
 'use strict';
 
-var bcrypt = require('bcrypt');
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var should = chai.should();
-var sinon = require('sinon');
+const bcrypt = require('bcrypt');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const should = chai.should();
+const sinon = require('sinon');
 
 require('sinon-as-promised');
 chai.use(chaiAsPromised);
 require('mongoose').Promise = Promise;
 
-var testUtils = require('./utils');
-var User = require('../../models/userModel');
-var utils = require('../../models/userUtils');
+const testUtils = require('./utils');
+const User = require('../../models/userModel');
+const utils = require('../../models/userUtils');
 
-describe('User Model', function() {
-  var sandbox;
-  var next;
+describe('User Model', () => {
+  let sandbox;
+  let next;
 
   before(testUtils.connect);
 
-  beforeEach(function(done) {
+  beforeEach((done) => {
     sandbox = sinon.sandbox.create();
     next = sandbox.spy();
 
-    var user1 = new User({
+    const user1 = new User({
       username: 'amy',
       email: 'amy@journey.com',
       password: 'abc123'
-    });
+    }).save();
 
-    var user2 = new User({
+    const user2 = new User({
       username: 'alex',
       email: 'alex@journey.com',
       password: 'abc123'
-    });
+    }).save();
 
-    user1.save(function() {
-      user2.save(function() {
-        done();
-      });
-    });
+    Promise.all([user1, user2]).then(() => done());
   });
 
-  afterEach(function(done) {
+  afterEach((done) => {
     sandbox.restore();
     User.remove({}, done);
   });
 
-  describe('#create:', function() {
-    it('creates a new User given the right fields', function(done) {
-      var testUser = new User({
+  describe('#create:', () => {
+    it('creates a new User given the right fields', (done) => {
+      const testUser = new User({
         username: 'herman',
         password: 'abc123',
         email: 'herman@journey.com',
         name: 'Herman Ng'
       });
 
-      testUser.save(function(err, user) {
+      testUser.save((err, user) => {
         should.not.exist(err);
         (typeof user).should.equal('object');
         user.username.should.equal(testUser.username);
@@ -71,23 +67,23 @@ describe('User Model', function() {
     });
   });
 
-  describe('#pre-save checks:', function() {
-    context('#duplicate field checking:', function() {
-      var field = 'foo';
-      var value = 'bar';
-      var stubModel;
+  describe('#pre-save checks:', () => {
+    context('#duplicate field checking:', () => {
+      const field = 'foo';
+      const value = 'bar';
+      let stubModel;
 
-      beforeEach(function() {
-        stubModel = { isModified: function() { return true; } };
+      beforeEach(() => {
+        stubModel = { isModified: () => { return true; } };
       });
 
-      it('calls Model.count() to check if the field exists', function(done) {
-        stubModel.model = function() {
+      it('calls Model.count() to check if the field exists', (done) => {
+        stubModel.model = () => {
           return {
-            count: function(opts) {
+            count: (opts) => {
               opts[field].should.equal(value);
               Object.keys(opts).length.should.equal(1);
-              return { exec: function() { return Promise.resolve(0); } };
+              return { exec: () => { return Promise.resolve(0); } };
             }
           };
         };
@@ -95,68 +91,52 @@ describe('User Model', function() {
         utils.fieldExistsCheck.bind(stubModel)(field, value, done);
       });
 
-      it('fails if Model.count() returns an error', function(done) {
-        var stubError = 'count() error';
-        var stubNext = function(err) {
+      it('fails if Model.count() returns an error', (done) => {
+        const stubError = 'count() error';
+        const stubNext = (err) => {
           err.should.eql(stubError);
           done();
         };
 
-        stubModel.model = function() {
-          return {
-            count: function() {
-              return {
-                exec: function() {
-                  return Promise.reject(stubError);
-                }
-              };
-            }
-          };
+        stubModel.model = () => {
+          return ({ count: () => ({ exec: () => Promise.reject(stubError)}) });
         };
 
         utils.fieldExistsCheck.bind(stubModel)(field, value, stubNext);
       });
 
-      it('moves on if the field has not changed', function() {
-        stubModel.isModified = function() { return false; };
+      it('moves on if the field has not changed', () => {
+        stubModel.isModified = () => { return false; };
         utils.fieldExistsCheck.bind(stubModel)(field, null, next);
         next.calledOnce.should.equal(true);
       });
 
-      it('returns an error if the field exists', function(done) {
-        var stubError = new Error(field + ' already exists');
-        var stubNext = function(err) {
+      it('returns an error if the field exists', (done) => {
+        const stubError = new Error(field + ' already exists');
+        const stubNext = (err) => {
           err.should.eql(stubError);
           done();
         };
 
-        stubModel.model = function() {
-          return {
-            count: function() {
-              return {
-                exec: function() {
-                  return Promise.resolve(1);
-                }
-              };
-            }
-          };
+        stubModel.model = () => {
+          return ({ count: () => ({ exec: () => Promise.resolve(1) }) });
         };
 
         utils.fieldExistsCheck.bind(stubModel)(field, value, stubNext);
       });
     });
 
-    context('#password length check:', function() {
-      it('moves on if the password is >= 6 characters', function() {
-        var stubModel = { password: 'abc123' };
+    context('#password length check:', () => {
+      it('moves on if the password is >= 6 characters', () => {
+        const stubModel = { password: 'abc123' };
 
         utils.checkPasswordLength.bind(stubModel)(next);
         next.calledOnce.should.equal(true);
       });
 
-      it('returns an error if password is < 6 characters', function() {
-        var error = new Error('Password needs to be at least 6 characters');
-        var stubModel = { password: 'a1b2' };
+      it('returns an error if password is < 6 characters', () => {
+        const error = new Error('Password needs to be at least 6 characters');
+        const stubModel = { password: 'a1b2' };
 
         utils.checkPasswordLength.bind(stubModel)(next);
         next.calledOnce.should.equal(true);
@@ -164,18 +144,18 @@ describe('User Model', function() {
       });
     });
 
-    context('#password hashing:', function() {
-      var stubModel;
+    context('#password hashing:', () => {
+      let stubModel;
 
-      beforeEach(function() {
+      beforeEach(() => {
         stubModel = {
           password: 'abc123',
-          isModified: function() { return true; }
+          isModified: () => { return true; }
         };
       });
 
-      it('hashes the user password before storing to db', function(done) {
-        sandbox.stub(bcrypt, 'hash', function(password) {
+      it('hashes the user password before storing to db', (done) => {
+        sandbox.stub(bcrypt, 'hash', (password) => {
           password.should.equal(stubModel.password);
           done();
         });
@@ -183,16 +163,16 @@ describe('User Model', function() {
         utils.hashPassword.bind(stubModel)();
       });
 
-      it('only pre-processes if the password has changed', function() {
-        stubModel.isModified = function() { return false; };
+      it('only pre-processes if the password has changed', () => {
+        stubModel.isModified = () => { return false; };
 
         utils.hashPassword.bind(stubModel)(next);
         next.calledOnce.should.equal(true);
       });
 
-      it('fails if the hash function returns an error', function(done) {
-        var stubError = 'bcrypt.hash() error';
-        sandbox.stub(bcrypt, 'hash', function(pw, saltCount, cb) {
+      it('fails if the hash function returns an error', (done) => {
+        const stubError = 'bcrypt.hash() error';
+        sandbox.stub(bcrypt, 'hash', (pw, saltCount, cb) => {
           cb(stubError);
           next.calledWith(stubError).should.equal(true);
           done();
@@ -201,10 +181,10 @@ describe('User Model', function() {
         utils.hashPassword.bind(stubModel)(next);
       });
 
-      it('sets the password if hash function succeeds', function(done) {
-        var stubHash = 'ABC!@#';
+      it('sets the password if hash function succeeds', (done) => {
+        const stubHash = 'ABC!@#';
 
-        sandbox.stub(bcrypt, 'hash', function(pw, saltCount, cb) {
+        sandbox.stub(bcrypt, 'hash', (pw, saltCount, cb) => {
           cb(null, stubHash);
 
           next.calledOnce.should.equal(true);
@@ -217,44 +197,36 @@ describe('User Model', function() {
     });
   });
 
-  describe('#validation:', function() {
-    it('fails if password is not provided', function(done) {
-      var testUser = new User({
+  describe('#validation:', () => {
+    it('fails if password is not provided', () => {
+      const testUser = new User({
         username: 'herman',
         email: 'herman@journey.com'
       });
 
-      testUser.validate(function(err) {
-        should.exist(err);
-        err.name.should.equal('ValidationError');
-        done();
-      });
+      testUser.validate().should.eventually.be.rejected;
     });
 
-    it('fails if email is not provided', function(done) {
-      var testUser = new User({
+    it('fails if email is not provided', () => {
+      const testUser = new User({
         username: 'herman',
         password: 'abc123'
       });
 
-      testUser.validate(function(err) {
-        should.exist(err);
-        err.name.should.equal('ValidationError');
-        done();
-      });
+      testUser.validate().should.eventually.be.rejected;
     });
   });
 
-  describe('#instance methods:', function() {
-    context('#checkPassword:', function() {
-      it('calls bcrypt.compareSync() with the right arguments', function() {
-        var newPassword = '123abc';
-        var oldPassword = 'abc123';
-        var bcryptMock = sandbox.mock(bcrypt)
+  describe('#instance methods:', () => {
+    context('#checkPassword:', () => {
+      it('calls bcrypt.compareSync() with the right arguments', () => {
+        const newPassword = '123abc';
+        const oldPassword = 'abc123';
+        const bcryptMock = sandbox.mock(bcrypt)
                                 .expects('compareSync')
                                 .withArgs(newPassword, oldPassword);
 
-        var testUser = new User({
+        const testUser = new User({
           username: 'herman',
           password: oldPassword
         });
